7572e18b132ccd95cf15ad39926208d6ecd243c7
Jeremy <the.jeremy.cobb@gmail.com>
2023-11-14 14:04:04 -0600

    added bootstrap buttons to cart area

--- src/App.jsx
+++ src/App.jsx
@@ -115,7 +115,21 @@ const App = () => {
                                     <ul>
                                         {state.items.map((itemlog, idx) => (
                                             <li key={`item-${itemlog.id}`}>
-                                                {itemlog.item}: ${itemlog.price}
+                                                {itemlog.item}: ${itemlog.price} 
+                                                <div className="btn-group m-1">
+                                                    <button type="button" className="btn btn-outline-success">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square-fill" viewBox="0 0 16 16">
+                                                            <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z" />
+                                                        </svg>
+                                                        <span className="visually-hidden">Button</span>
+                                                    </button>
+                                                    <button type="button" className="btn btn-outline-danger">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-square-fill" viewBox="0 0 16 16">
+                                                            <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm2.5 7.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1z" />
+                                                        </svg>
+                                                        <span className="visually-hidden">Button</span>
+                                                    </button>
+                                                </div>
                                             </li>
                                         ))}
                                     </ul>
